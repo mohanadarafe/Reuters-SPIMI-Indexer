aa829fa4f5e57c32be41000e6254bb5ea40e9d57
Mohanad <marafe@live.com>
2020-11-15 13:12:07 -0500

    Merging blocks

--- subproject1/subproject1.py
+++ subproject1/subproject1.py
@@ -1,5 +1,5 @@
 import nltk, time, sys, os, json
-from collections import OrderedDict 
+from collections import OrderedDict, defaultdict 
 from tqdm import tqdm
 from heapq import merge
 sys.path.append('utils')
@@ -22,9 +22,9 @@ def build_postings_list(block, dictionary):
         
     return dictionary
 
-def read_block_from_disk(blockNumber: int) -> dict:
+def read_block_from_disk(blockFile: str) -> dict:
     dictionary = dict()
-    with open(f"output/block{blockNumber}.json", 'r') as json_file: 
+    with open(f"output/{blockFile}", 'r') as json_file: 
         dictionary = json.load(json_file)
 
     return dictionary
@@ -35,25 +35,26 @@ def write_block_to_disk(dictionary: dict, blockNumber: int):
         fp.write(str(raw))
 
 def merge_blocks():
-    NUM_OF_BLOCKS = len(os.listdir("output"))
-    dictionary = read_block_from_disk(1)
-
+    inverted_index = OrderedDict()
+    
     print("Merging blocks...")
-    for i in tqdm(range(2, NUM_OF_BLOCKS)):
-        current_dict = read_block_from_disk(i)
+    for blockfile in tqdm(os.listdir("output")):
+        block = read_block_from_disk(blockfile)
+        for key in block:
+            postings = inverted_index[key] if key in inverted_index else set()
+            doc_occurences = block[key][1]
+            postings.update(doc_occurences)
+            inverted_index[key] = postings
 
-        for term in current_dict.keys():
-            if term not in dictionary:
-                dictionary[term] = current_dict[term]
-            elif (term in dictionary) and (current_dict[term][0] not in dictionary[term][1]):
-                dictionary[term][1].append(current_dict[term][0])
-                dictionary[term][0] = len(dictionary[term][1])
+    print("Computing doc frequency...")
+    for token in tqdm(inverted_index):
+        sorted_doc_freq = sorted(inverted_index[token])
+        doc_freq = len(sorted_doc_freq)
+        inverted_index[token] = (doc_freq, sorted_doc_freq)
 
-    raw = json.dumps(dictionary)
-    with open(f'output/merged_list.json', 'w') as fp:
-        fp.write(str(raw))
-
-    print(f'The final postings list contains {len(dictionary)} terms')
+    raw = json.dumps(inverted_index)
+    with open('postings_list.json', 'w') as f:
+        f.write(str(raw))
 
 def separate_blocks(path):
     print("\nGeting raw files...")
@@ -85,4 +86,4 @@ def separate_blocks(path):
     write_block_to_disk(dictionary, blockNumber)
     merge_blocks()
 
-separate_blocks(args.path)
+merge_blocks()
